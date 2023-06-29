e7158c12d9716041ebeb3b41d379db8b1a4d3fd6
Grincode <juanpetarcich@gmail.com>
2023-06-29 01:37:16 -0300

    Models
    creacion de usuario modelo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'GlobalIso';
+  title = 'GlobalIsso';
+
+  public user: User;
+
+  constructor() {
+    this.user = {
+      username: 'Admin',
+      email: 'admin@admin.com',
+      isAdmin: true,
+    };
+  }
 }
